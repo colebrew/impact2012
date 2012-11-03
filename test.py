13315c09d1c4d6c3fb49e267c4105402118c57e5
Kevin Tan <kevitan@umich.edu>
2012-11-02 22:27:28 -0400

    added regexp search for 'exam'

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ child = subprocess.Popen('pdf2txt.py Syllabus3.pdf > output.txt', shell=True)
 raw = open('output.txt')
 lines = []
 for line in raw:
-    if "" == line.strip() || re.search(:
+    if "" == line.strip() or re.search(r'\b(?i)exam(?-i)'== 0:
         continue
     lines.append(line.strip())
     print line,
