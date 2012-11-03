f08a66dca31f0d9d0eb8f67f5dc94b6e968623d9
Nick Otto <nwkotto@umich.edu>
2012-11-02 23:11:33 -0400

    Added some files

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import subprocess
+import os
+import csv
 
 child = subprocess.Popen('pdf2txt.py Syllabus3.pdf > output.txt', shell=True)
 raw = open('output.txt')
 lines = []
 for line in raw:
-    if "" == line.strip() or re.search(r'\b(?i)exam(?-i)'== 0:
+    if "" == line.strip() or re.search(r'\b(?i)exam(?-i)'== 0):
         continue
     lines.append(line.strip())
     print line,
