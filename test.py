1f3138907dc506c2394690334f40f49ea2fdea87
Kevin Tan <kevitan@umich.edu>
2012-11-02 23:03:53 -0400

    regex search for exam works... not sure what that accomplishes. code works at least

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import subprocess
+import re
 
 child = subprocess.Popen('pdf2txt.py Syllabus3.pdf > output.txt', shell=True)
 raw = open('output.txt')
 lines = []
 for line in raw:
-    if "" == line.strip() or re.search(r'\b(?i)exam(?-i)', line)== 0:
+    re.I
+    if ("" == line.strip()) or (re.search(re.compile('(?<=exam)'), line) == None):
         continue
     lines.append(line.strip())
     print line,
